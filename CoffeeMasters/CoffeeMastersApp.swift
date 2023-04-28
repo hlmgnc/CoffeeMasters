0e0f3b5bbc62955082ed58e353a20d8414411277
hlmgnc <61838067+hlmgnc@users.noreply.github.com>
2023-04-28 10:02:28 +0200

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Hilmi Genc on 28.04.23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
